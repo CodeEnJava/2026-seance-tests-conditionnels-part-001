30edc929bc8906bb0e41e16a59b44f0be786d9b0
stebar <steph.barois.dev@gmail.com>
2026-08-25 10:44:45 +0200

    Réalisation de l'exemple N°3 pour l'utilisation de if  elif et else

--- choix_3.py
+++ choix_3.py
@@ -1,1 +1,26 @@
 # code pour exercice N°3
+"""
+On souhaite indiquer si un nombre entier est :
+•	Négatif
+•	Positif et impair
+•	Positif et pair
+
+en utilisant if else
+"""
+print("Test du choix N°3 : if - elif:")
+# Lire un nombre entier au clavier
+while True:
+    try:
+        nombre = int(input("Entrer un nombre entier :"))
+        break
+    except ValueError:
+        print("Erreur ! le nombre saisi n'est pas valide")
+
+# fin de la lecture du nombre entier
+
+if nombre < 0:
+    print("%d est un nombre négatif" %(nombre))
+elif nombre % 2 ==0:
+    print("%d est un nombre positif et pair")
+else:
+    print("%d est un nombre positif et impair" %(nombre))
