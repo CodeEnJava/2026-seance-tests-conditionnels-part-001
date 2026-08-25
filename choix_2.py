e6c34cee32c7da1335ddb06e38e0e50aec2bfed9
stebar <steph.barois.dev@gmail.com>
2026-08-25 14:12:17 +0200

    Ajout du code pour les choix_1, choix_2 et choix_3

--- choix_2.py
+++ choix_2.py
@@ -1,2 +1,30 @@
 # Code pour l'exercice n°2
+# Code pour l'exercice n°2
 """
+On souhaite indiquer si un nombre entier est :
+•	Négatif
+•	Positif et impair
+•	Positif et pair
+
+en utilisant if else
+"""
+print("Test du choix N°2 : if - else:")
+# Lire un nombre entier au clavier
+while True:
+    try:
+        nombre = int(input("Entrer un nombre entier :"))
+        break
+    except ValueError:
+        print("Erreur ! le nombre saisi n'est pas valide")
+
+# fin de la lecture du nombre entier
+
+# traitement #
+
+if nombre < 0:
+    print("%d est un nombre négatif" %(nombre))
+else:
+    if nombre%2==0:
+        print("%d le nombre est positif et pair" % (nombre))
+    else:
+        print("%d le nombre est positif et impair" % (nombre))
